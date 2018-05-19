cf984aac59216822f5971fd0b95294178fa78ae6
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-05-19 20:55:30 +0200

    Vote validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :hack
+
+  # Cannot create a new `Vote` without a `user` and a `hack`
+  validates :user, presence: true
+  validates :hack, presence: true
 end
