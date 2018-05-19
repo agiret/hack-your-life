fee48e962218405f6f0d354c030b1cdd072108d7
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-05-19 20:53:14 +0200

    Hack validations

--- app/models/hack.rb
+++ app/models/hack.rb
@@ -1,4 +1,13 @@
 class Hack < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  # Cannot create a new `Hack` without a `link`, `category`and `user`
+  validates :link, presence: true
+  validates :category, presence: true
+  validates :user, presence: true
+
+  # Cannot create two hacks with the same `link`
+  validates :link, uniqueness: true
+
 end
