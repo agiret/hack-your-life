576f98f9fedabad17bc6f287689b3071d4c4c60a
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-05-19 19:44:38 +0200

    Category validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,11 @@
 class Category < ApplicationRecord
+  # Cannot create a new `Category` without a `name`
+  validates :name, presence: true
+
+  # Cannot create two categories with the same `name`
+  validates :name, uniqueness: true
+
+  # `name` has to be one of the options
+  # validates :name, inclusion: { in: ["sports", "clothing", "technology"] }
+
 end
