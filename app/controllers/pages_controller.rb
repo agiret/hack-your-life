21166bb2f28f3984411422312341c302d9e12316
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-05-21 22:41:48 +0200

    Premier seed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @hacks = Hack.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p 'Suppression des données existances...'
+Hack.destroy_all
+Category.destroy_all
+User.destroy_all
+
+p 'Créations du jeu de données...'
+# Categories
+p '-> Categories'
+CAT = []
+CATEGORIES = ["Cuisine", "Bricolage", "Ménage"]
+CATEGORIES.each do |categroy|
+  cat = Category.create!(name: categroy)
+  CAT << cat
+end
+
+# Users
+p '-> Users'
+User.create!(email: "user1@user.fr", password: "azerty")
+User.create!(email: "user2@user.fr", password: "azerty")
+
+# Hacks
+p '-> Hacks'
+Hack.create!(link: "www.google.com", user_id: User.first.id, category_id: CAT[0].id)
+Hack.create!(link: "www.lemonde.fr", user_id: User.last.id, category_id: CAT[1].id)
+Hack.create!(link: "www.lewagon.com", user_id: User.first.id, category_id: CAT[0].id)
+
+p 'Jeu de données créé !'
